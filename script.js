09fed665a3be5a7e6685a0bc1c5cee670f5fa00e
ElChicoEevee <kirbyyeray@hotmail.com>
2022-08-29 04:35:11 +0200

    Merged with @EoF-1141 PR i just saw

---
+++ script.js
@@ -0,0 +1,43 @@
+const app = document.getElementById("app") 
+
+const percent_types = ["Crit. Chance", "CDR"]
+
+for (let badge of badges) {
+	let a = document.createElement('div')
+	a.classList.add('animal')
+	let t = `
+	<p><strong>Pokémon:</strong> ${badge.mon}</p>
+    <img class="pet-photo" src="images/${badge.ico}.png">
+	`
+	let pos = {}, neg = {};
+
+	for (let k in badge.stat) {
+		let v = badge.stat[k]
+		if (v < 0) {
+			neg[k] = v
+		} else {
+			pos[k] = v
+		}
+	}
+	for (let k in pos) {
+		let v = pos[k]
+		if (percent_types.includes(k)) {
+			t += `<p class="pos"><strong>${k}:</strong> ${v/100}%</p>`
+		} else {
+			t += `<p class="pos"><strong>${k}:</strong> ${v}</p>`
+		}
+	}
+	for (let k in neg) {
+		let v = neg[k]
+		if (percent_types.includes(k)) {
+			t += `<p class="neg"><strong>${k}:</strong> ${v/100}%</p>`
+		} else {
+			t += `<p class="neg"><strong>${k}:</strong> ${v}</p>`
+		}
+	}
+	for (let x of badge.type) {
+		t += `<img src="images/${x}.png">`
+	}
+	a.innerHTML = t
+	app.appendChild(a)
+}
